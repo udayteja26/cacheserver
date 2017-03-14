3fd1e60fc343861580eed4369313c5c29dd8e108
kck1710 <chaitu24250@gmail.com>
2017-03-14 11:08:34 +0530

    first commit

---
+++ cache_server/test.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install apt-cacher-ng
+sudo chmod 777 /etc/apt-cacher-ng/acng.conf
+sudo cat test1.conf > /etc/apt-cacher-ng/acng.conf
+sudo /etc/init.d/apt-cacher-ng restart
+#etc.
+
