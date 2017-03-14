ce3646b5760174edc95793ac49ccb6ff090f48a2
Chaitanya Krishna Kasaraneni <chaitu24250@gmail.com>
2017-03-14 18:38:48 +0530

    update

--- cache_server/test.sh
+++ cache_server/test.sh
@@ -5,5 +5,6 @@ sudo apt-get install apt-cacher-ng
 sudo chmod 777 /etc/apt-cacher-ng/acng.conf
 sudo cat test1.conf > /etc/apt-cacher-ng/acng.conf
 sudo /etc/init.d/apt-cacher-ng restart
+sudo chmod 755 /etc/apt-cacher-ng/acng.conf
 #etc.
 
